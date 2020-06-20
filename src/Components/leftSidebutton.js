09aba422ba9c309a3a0de108c2072b9432e430cf
JWeez <jwellstx@gmail.com>
2020-06-20 11:24:40 -0500

    commented out example code in button page

--- src/Components/leftSidebutton.js
+++ src/Components/leftSidebutton.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { green, purple, orange } from '@material-ui/core/colors';
-import API from "../utils/API;
+import API from "../utils/API";
 
 const BootstrapButton = withStyles({
   root: {
@@ -64,21 +64,6 @@ const theme = createMuiTheme({
   },
 });
 
-class Button extends Component {
-  componentDidMount() {
-    API.getW3Refrence("css")
-       .then(res => {
-         this.setState({references: res})
-       });
-  }
-
-  render() { 
-    return ( 
-      this.state.references.map((key) => <Button reference={key}></Button>)
-     );
-  }
-}
-
 
 export default function CustomizedButtons() {
   const classes = useStyles();
@@ -99,3 +84,19 @@ export default function CustomizedButtons() {
     </div>
   );
 }
+
+
+// class Button extends Component {
+//   componentDidMount() {
+//     API.getW3Refrence("css")
+//        .then(res => {
+//          this.setState({references: res})
+//        });
+//   }
+
+//   render() { 
+//     return ( 
+//       this.state.references.map((key) => <Button reference={key}></Button>)
+//      );
+//   }
+// }
