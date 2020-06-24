c70ef367ae7f695f258441f497a9a527f5b470d3
JWeez <jwellstx@gmail.com>
2020-06-23 20:21:56 -0500

    spelling mistake

--- controllers/youtubeController.js
+++ controllers/youtubeController.js
@@ -4,7 +4,7 @@ module.exports = {
   // find all books
   getVideos: function (req, res) {
     console.log("inside getVideos");
-    const searchTopic = req.query.topic ? req.query.topic.replace(/\s/, "%20") : "full%20stack%20develoment";
+    const searchTopic = req.query.topic ? req.query.topic.replace(/\s/, "%20") : "full%20stack%20development";
     axios
       .get(
         "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=" + searchTopic + "&type=video&key=AIzaSyCigK945eyJAItwigHVvdihdi4x9NcRv4k"
@@ -12,7 +12,7 @@ module.exports = {
       .then((response) => {
         let videos = response.data.items;
         let results = [];
-        
+
         for (let x = 0 ; x <= 4 ; x++ ) {  // get 5 random videos from 30 total
             let randomIndex = Math.floor(Math.random() * videos.length); 
             results.push({
